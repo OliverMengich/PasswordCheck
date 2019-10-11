f8730b10f5d93c3d53c871b6174653865a8df10a
OliverMengich <kemeioliver2000@gmail.com>
2019-10-11 16:20:41 +0300

    Password Check Out

---
+++ PasswordCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PasswordCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Oliver";
+            string Password = "Oliver8677";
+            string _password;
+            string _name;
+            Console.WriteLine("Enter your name");
+            _name = Console.ReadLine();
+            Console.WriteLine("Enter your Password");
+            _password = Console.ReadLine();
+            bool Islooping = false;
+            do
+            {
+                if (_password == Password)
+                {
+                    Console.WriteLine("Correct");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry");
+                }
+            } while (Islooping);
+           
+            
+        
+        }
+    }
+}
